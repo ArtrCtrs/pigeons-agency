5d786fa1128805b0783fc4387d30fbd5f84b7e47
unknown <arthur.catrisse@hotmail.com>
2019-02-16 17:12:51 +0100

    home page dynamic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { PageDataService, HomePageDataAPIReturn } from './../../services/page-data.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
     selector: 'app-home',
@@ -9,12 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
     pageLoading: boolean = true;
-    pageData: HomePageDataAPIReturn;
+    user:User;
+    interval:any;
 
     constructor(private PageDataService: PageDataService) { }
 
     async ngOnInit() {
-        this.pageData = await this.PageDataService.getHomePageData();
+        this.user = (await this.PageDataService.getHomePageData()).data;
         this.pageLoading = false;
+        this.interval = setInterval(() => { this.upDateFrontInfo(); }, 1000);
+    }
+
+    upDateFrontInfo() {
+        console.log("m")
+        this.user.seeds = this.user.seeds < this.user.maxseeds ? this.user.seeds + (this.user.seedsminute / 60) : this.user.maxseeds;
+        this.user.droppings = this.user.droppings < this.user.maxdroppings ? this.user.droppings + (this.user.totaldroppingsminute / 60) : this.user.maxdroppings;
+    }
+
+    ngOnDestroy(){
+        clearInterval(this.interval);
     }
 }
