2827e3763e6d870024d347e7c2b5c68f670da062
Arthur Ctrs <arthur.catrisse@hotmail.com>
2019-02-23 15:12:30 +0100

    start map update

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as L from 'leaflet';
+import { User } from 'src/app/interfaces/user';
+import { PageDataService } from 'src/app/services/page-data.service';
 
 @Component({
     selector: 'app-map',
@@ -8,24 +10,47 @@ import * as L from 'leaflet';
     styleUrls: ['./map.component.less']
 })
 export class MapComponent implements OnInit {
+    pageLoading: boolean = true;
+    allusers: User[];
+    pigeon_map: any;
 
     private drawnItems: L.FeatureGroup;
 
-    constructor() { }
+    constructor(private PageDataService: PageDataService) { }
 
     ngOnInit() {
-        const pigeon_map = L.map('pigeon_map').setView([5, 5], 4);
+        this.pigeon_map = L.map('pigeon_map').setView([5, 5], 4);
+        console.log(this.pigeon_map);
 
         L.tileLayer('/assets/tiles/{z}/{x}/{y}.png', {
             minZoom: 4,
             maxZoom: 4,
-            attribution: 'Carte des pigeons'
-        }).addTo(pigeon_map);
+            attribution: 'Players Map'
+        }).addTo(this.pigeon_map);
 
         let pigeonIcon = L.icon({
             iconUrl: '/assets/marker/marker_pigeon.png'
         });
 
-        let pigeon_marker = L.marker([5, 5], { icon: pigeonIcon }).addTo(pigeon_map);
+        let pigeon_marker = L.marker([5, 5], { icon: pigeonIcon }).addTo(this.pigeon_map);
+
+        this.updateMapInfo()
+        this.pageLoading = false;
+    }
+
+    async updateMapInfo() {
+        let pigeonIcon = L.icon({
+            iconUrl: '/assets/marker/marker_pigeon.png'
+        });
+
+        this.allusers = (await this.PageDataService.getLeaderboardData()).data;
+        this.allusers.forEach(function (user) {
+            console.log(user)
+            L.marker([1.5, 5], { icon: pigeonIcon }).on('mouseover', this.onHover).addTo(this.pigeon_map);
+        }, this);
+    }
+
+    onHover() {
+        console.log("yee")
     }
 }
