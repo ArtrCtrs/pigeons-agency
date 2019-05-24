0bdab5bc6d5f427576fd3330baa4659f12e41e8f
Arthur Ctrs <arthur.catrisse@hotmail.com>
2019-05-24 19:43:08 +0200

    handle level limitations for upgrades

--- src/app/lists/seedsStorageList.ts
+++ src/app/lists/seedsStorageList.ts
@@ -1,105 +1,33 @@
 const seedsStorageList = [
-    {
-        lvl: 0,
-        droppingsCost: 0,
-        feathersCost:0,
-        seedsStorage: 200
-    },
-    {
-        lvl: 1,
-        droppingsCost: 80,
-        feathersCost:2,
-        seedsStorage: 600
-    },
-    {
-        lvl: 2,
-        droppingsCost: 200,
-        feathersCost:8,
-        seedsStorage: 1000
-    },
-    {
-        lvl: 3,
-        droppingsCost: 450,
-        feathersCost:18,
-        seedsStorage: 2000
-    },
-    {
-        lvl: 4,
-        droppingsCost: 3000,
-        feathersCost:50,
-        seedsStorage: 3000
-    }
-    ,
-    {
-        lvl: 5,
-        droppingsCost: 6000,
-        feathersCost:100,
-        seedsStorage: 5000
-    },
-    {
-        lvl: 6,
-        droppingsCost: 12000,
-        feathersCost:130,
-        seedsStorage: 7000
-    },
-    {
-        lvl: 7,
-        droppingsCost: 24000,
-        feathersCost:180,
-        seedsStorage: 10000
-    },
-    {
-        lvl: 8,
-        droppingsCost: 48000,
-        feathersCost:250,
-        seedsStorage: 13000
-    },
-    {
-        lvl: 9,
-        droppingsCost: 96000,
-        feathersCost:300,
-        seedsStorage: 18000
-    },
-    {
-        lvl: 10,
-        droppingsCost: 200000,
-        feathersCost:320,
-        seedsStorage: 25000
-    },
-    {
-        lvl: 11,
-        droppingsCost: 600000,
-        feathersCost:340,
-        seedsStorage: 32000
-    }
-    ,
-    {
-        lvl: 12,
-        droppingsCost: 2000000,
-        feathersCost:400,
-        seedsStorage: 40000
-    },
-    {
-        lvl: 13,
-        droppingsCost: 3500000,
-        feathersCost:700,
-        seedsStorage: 49000
-    },
-    {
-        lvl: 14,
-        droppingsCost: 5200000,
-        feathersCost:1300,
-        seedsStorage: 60000
-    },
-    {
-        lvl: 15,
-        droppingsCost: 9999999999,
-        feathersCost:999999999,
-        seedsStorage: 999999999
-    }
-    
+{lvl: 0,droppingsCost: 0,feathersCost:0,seedsStorage: 25 },
+{lvl: 1,droppingsCost: 30,feathersCost:10,seedsStorage: 50 },
+{lvl: 2,droppingsCost: 150,feathersCost:20,seedsStorage: 110 },
+{lvl: 3,droppingsCost: 500,feathersCost:30,seedsStorage: 220 },
+{lvl: 4,droppingsCost: 1300,feathersCost:40,seedsStorage: 420 },
+{lvl: 5,droppingsCost: 3300,feathersCost:50,seedsStorage: 950 },
+{lvl: 6,droppingsCost: 10000,feathersCost:60,seedsStorage: 1600 },
+{lvl: 7,droppingsCost: 27000,feathersCost:70,seedsStorage: 2600 },
+{lvl: 8,droppingsCost: 63000,feathersCost:80,seedsStorage: 4100 },
+{lvl: 9,droppingsCost: 149000,feathersCost:90,seedsStorage: 6100 },
+{lvl: 10,droppingsCost: 320000,feathersCost:100,seedsStorage: 8400 },
+{lvl: 11,droppingsCost: 662000,feathersCost:110,seedsStorage: 12800 },
+{lvl: 12,droppingsCost: 1271000,feathersCost:120,seedsStorage: 15900 },
+{lvl: 13,droppingsCost: 2200000,feathersCost:130,seedsStorage: 19600 },
+{lvl: 14,droppingsCost: 2588000,feathersCost:140,seedsStorage: 23300 },
+{lvl: 15,droppingsCost: 3278000,feathersCost:150,seedsStorage: 28200 },
+{lvl: 16,droppingsCost: 4026000,feathersCost:160,seedsStorage: 32900 },
+{lvl: 17,droppingsCost: 4831000,feathersCost:170,seedsStorage: 38000 },
+{lvl: 18,droppingsCost: 5693000,feathersCost:180,seedsStorage: 42800 },
+{lvl: 19,droppingsCost: 6613000,feathersCost:190,seedsStorage: 48000 },
+{lvl: 20,droppingsCost: 15872000,feathersCost:200,seedsStorage: 66000 },
+{lvl: 21,droppingsCost: 18689000,feathersCost:210,seedsStorage: 78000 },
+{lvl: 22,droppingsCost: 21680000,feathersCost:220,seedsStorage: 90000 },
+{lvl: 23,droppingsCost: 24842000,feathersCost:230,seedsStorage: 102000 },
+{lvl: 24,droppingsCost: 28178000,feathersCost:240,seedsStorage: 114000 },
+{lvl: 25,droppingsCost: 31685000,feathersCost:250,seedsStorage: 138000 },
+{lvl: 26,droppingsCost: 53048000,feathersCost:260,seedsStorage: 162000 },
+{lvl: 27,droppingsCost: 58827000,feathersCost:270,seedsStorage: 186000 },
+{lvl: 28,droppingsCost: 64865000,feathersCost:280,seedsStorage: 210000 },
+{lvl: 29,droppingsCost: 71162000,feathersCost:290,seedsStorage: 230000 },
 ]
-
 export default seedsStorageList;
-
-
--- src/app/lists/seedsUpgradesList.ts
+++ src/app/lists/seedsUpgradesList.ts
@@ -1,101 +1,34 @@
-const seedsUpgradesList = [
-    {
-        lvl: 0,
-        droppingsCost: 0,
-        newSeeds: 30
-    },
-    {
-        lvl: 1,
-        droppingsCost: 20,
-        newSeeds: 40
-    },
-    {
-        lvl: 2,
-        droppingsCost: 80,
-        newSeeds: 60
-    },
-    {
-        lvl: 3,
-        droppingsCost: 200,
-        newSeeds: 80
-    },
-    {
-        lvl: 4,
-        droppingsCost: 400,
-        newSeeds: 120
-    }
-    ,
-    {
-        lvl: 5,
-        droppingsCost: 600,
-        newSeeds: 150
-    },
-    {
-        lvl: 6,
-        droppingsCost: 950,
-        newSeeds: 180
-    },
-    {
-        lvl: 7,
-        droppingsCost: 1400,
-        newSeeds: 220
-    },
-    {
-        lvl: 8,
-        droppingsCost: 2700,
-        newSeeds: 250
-    },
-    {
-        lvl: 9,
-        droppingsCost: 4800,
-        newSeeds: 300
-    },{
-        lvl: 10,
-        droppingsCost: 11000,
-        newSeeds: 450
-    },{
-        lvl: 11,
-        droppingsCost: 18000,
-        newSeeds: 850
-    },{
-        lvl: 12,
-        droppingsCost: 25000,
-        newSeeds: 1000
-    },{
-        lvl: 13,
-        droppingsCost: 35000,
-        newSeeds: 1300
-    },{
-        lvl: 14,
-        droppingsCost: 45000,
-        newSeeds: 1800
-    },{
-        lvl: 15,
-        droppingsCost: 55000,
-        newSeeds: 2400
-    },{
-        lvl: 16,
-        droppingsCost: 69000,
-        newSeeds: 2800
-    },{
-        lvl: 17,
-        droppingsCost: 99000,
-        newSeeds: 3350
-    },{
-        lvl: 18,
-        droppingsCost: 145000,
-        newSeeds: 3800
-    },{
-        lvl: 19,
-        droppingsCost: 199000,
-        newSeeds: 4350
-    },{
-        lvl: 20,
-        droppingsCost: 99999999,
-        newSeeds: 4350
-    }
+const seedsUpgradesList   = [
+{lvl: 0,droppingsCost: 0,newSeeds:60 },
+{lvl: 1,droppingsCost: 10,newSeeds:75 },
+{lvl: 2,droppingsCost: 20,newSeeds:90 },
+{lvl: 3,droppingsCost: 50,newSeeds:140 },
+{lvl: 4,droppingsCost: 130,newSeeds:200 },
+{lvl: 5,droppingsCost: 330,newSeeds:260 },
+{lvl: 6,droppingsCost: 1000,newSeeds:320 },
+{lvl: 7,droppingsCost: 2700,newSeeds:380 },
+{lvl: 8,droppingsCost: 6300,newSeeds:440 },
+{lvl: 9,droppingsCost: 15000,newSeeds:500 },
+{lvl: 10,droppingsCost: 32000,newSeeds:560 },
+{lvl: 11,droppingsCost: 66000,newSeeds:620 },
+{lvl: 12,droppingsCost: 127000,newSeeds:680 },
+{lvl: 13,droppingsCost: 220000,newSeeds:740 },
+{lvl: 14,droppingsCost: 260000,newSeeds:800 },
+{lvl: 15,droppingsCost: 330000,newSeeds:860 },
+{lvl: 16,droppingsCost: 400000,newSeeds:910 },
+{lvl: 17,droppingsCost: 480000,newSeeds:960 },
+{lvl: 18,droppingsCost: 570000,newSeeds:1000 },
+{lvl: 19,droppingsCost: 660000,newSeeds:1050 },
+{lvl: 20,droppingsCost: 1600000,newSeeds:1100 },
+{lvl: 21,droppingsCost: 1850000,newSeeds:1150 },
+{lvl: 22,droppingsCost: 2150000,newSeeds:1200 },
+{lvl: 23,droppingsCost: 2500000,newSeeds:1250 },
+{lvl: 24,droppingsCost: 2800000,newSeeds:1300 },
+{lvl: 25,droppingsCost: 3170000,newSeeds:1350 },
+{lvl: 26,droppingsCost: 5300000,newSeeds:1400 },
+{lvl: 27,droppingsCost: 5900000,newSeeds:1450 },
+{lvl: 28,droppingsCost: 6500000,newSeeds:1500 },
+{lvl: 29,droppingsCost: 7000000,newSeeds:1550 },
+{lvl: 30,droppingsCost: 11000000,newSeeds:1600 },
 ]
-
 export default seedsUpgradesList;
-
-
