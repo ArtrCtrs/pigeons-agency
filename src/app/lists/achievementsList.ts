4fef8acf595dbd8643c685c9ba7fb816f47ed7ae
ArtrCtrs <arthur.catrisse@hotmail.com>
2019-08-26 16:56:50 +0200

    Add files via upload

--- src/app/lists/achievementsList.ts
+++ src/app/lists/achievementsList.ts
@@ -14,10 +14,37 @@ const achievementslist    = [
     {id: "legendary500",name: "I did this without cheating ?",description:"Get 500 legendary pigeons (of your level)", value:500,attribute:"legendaries",reward:60},
 
     {id: "droppingsminute20",name: "Yes, guano",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
-    {id: "droppingsminute100",name: "Yes, guano",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
-    {id: "droppingsminute500",name: "Yes, guano",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
-    {id: "droppingsminute20",name: "Yes, guano",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
-    {id: "droppingsminute20",name: "Yes, guano",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
-    {id: "droppingsminute20",name: "Yes, guano",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
+    {id: "droppingsminute100",name: "Yes, guano",description:"Reach 100 droppings per minute", value:100,attribute:"droppingsminute",reward:20},
+    {id: "droppingsminute500",name: "Yes, guano",description:"Reach 500 droppings per minute", value:500,attribute:"droppingsminute",reward:30},
+    {id: "droppingsminute1000",name: "Yes, guano",description:"Reach 1000 droppings per minute", value:1000,attribute:"droppingsminute",reward:40},
+    {id: "droppingsminute5000",name: "Yes, guano",description:"Reach 5000 droppings per minute", value:5000,attribute:"droppingsminute",reward:50},
+    {id: "droppingsminute10000",name: "Yes, guano",description:"Reach 10000 droppings per minute", value:10000,attribute:"droppingsminute",reward:60},
+    {id: "droppingsminute20000",name: "Yes, guano",description:"Reach 20000 droppings per minute", value:20000,attribute:"droppingsminute",reward:80},
+
+    {id: "militaryscore10",name: "Noob",description:"Reach a military score of 10", value:10,attribute:"militaryscore",reward:10},
+    {id: "militaryscore30",name: "Yes, guano",description:"Reach a military score of 30", value:30,attribute:"militaryscore",reward:20},
+    {id: "militaryscore60",name: "Yes, guano",description:"Reach a military score of 60", value:60,attribute:"militaryscore",reward:30},
+    {id: "militaryscore100",name: "Yes, guano",description:"Reach a military score of 100", value:100,attribute:"militaryscore",reward:40},
+    {id: "militaryscore150",name: "Yes, guano",description:"Reach a military score of 150", value:150,attribute:"militaryscore",reward:50},
+    {id: "militaryscore200",name: "Yes, guano",description:"Reach a military score of 200", value:200,attribute:"militaryscore",reward:60},
+    {id: "militaryscore500",name: "Yes, guano",description:"Reach a military score of 500", value:500,attribute:"militaryscore",reward:80},
+
+    {id: "sell5",name: "This is hard",description:"Sell 5 pigeons", value:5,attribute:"soldpigeons",reward:10},
+    {id: "sell50",name: "Yes, feathers !",description:"Sell 50 pigeons", value:50,attribute:"soldpigeons",reward:20},
+    {id: "sell500",name: "Beginning of the end",description:"Sell 500 pigeons", value:500,attribute:"soldpigeons",reward:30},
+    {id: "sell5000",name: "You monster...",description:"Sell 5000 pigeons", value:5000,attribute:"soldpigeons",reward:40},
+
+    {id: "event1",name: "At least you tried",description:"Participate in an event", value:1,attribute:"eventparticipation",reward:10},
+    {id: "eventtop3",name: "Not useless",description:"Finish top 3 in an event", value:2,attribute:"eventparticipation",reward:20},
+    {id: "eventtop1",name: "Event champion",description:"Finish top 1 in an event", value:3,attribute:"eventparticipation",reward:30},
+
+    {id: "attack5",name: "Yes, guano",description:"Do 5 succcessful attacks", value:5,attribute:"totalwinattacks",reward:10},
+    {id: "attack50",name: "Yes, guano",description:"Do 50 succcessful attacks", value:50,attribute:"totalwinattacks",reward:20},
+    {id: "attack500",name: "Yes, guano",description:"Do 500 succcessful attacks", value:500,attribute:"totalwinattacks",reward:30},
+
+    {id: "defend5",name: "Yes, guano",description:"Do 5 succcessful defenses", value:5,attribute:"totalwindefences",reward:10},
+    {id: "defend50",name: "Yes, guano",description:"Do 50 succcessful defenses", value:50,attribute:"totalwindefences",reward:20},
+    {id: "defend500",name: "Yes, guano",description:"Do 500 succcessful defenses", value:500,attribute:"totalwindefences",reward:30},
+
 ]
     export default achievementslist ;
