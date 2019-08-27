14079bf138267f2a8482dcdcaf1d078ec6c68f52
ArtrCtrs <arthur.catrisse@hotmail.com>
2019-08-27 15:00:55 +0200

    add trade list

---
+++ src/app/lists/tradeList.ts
@@ -0,0 +1,7 @@
+const tradeList = [
+    {lvl: 0,fromHonor: 0,toFeathers:0,fromFeathers: 0,toDroppings:0 },
+    {lvl: 1,fromHonor: 10,toFeathers:0,fromFeathers: 30,toDroppings:0 },
+    {lvl: 2,fromHonor: 10,toFeathers:0,fromFeathers: 0,toDroppings:0 },
+    {lvl: 3,fromHonor: 10,toFeathers:0,fromFeathers: 0,toDroppings:0 },
+]
+export default tradeList;
