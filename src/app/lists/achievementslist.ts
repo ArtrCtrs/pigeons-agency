1bc5a9e650734a08979f3b85a2d9e001d0228f11
ArtrCtrs <arthur.catrisse@hotmail.com>
2019-08-27 16:59:29 +0200

    change achievement list

---
+++ src/app/lists/achievementslist.ts
@@ -0,0 +1,50 @@
+const achievementslist    = [
+    {id: "lvl2",name: "I understand the game !",description:"Get to lvl 2", value:2,attribute:"lvl",reward:10},
+    {id: "lvl5",name: "Leveling up !",description:"Get to lvl 5", value:5,attribute:"lvl",reward:20},
+    {id: "lvl10",name: "Getting serious.",description:"Get to lvl 10", value:10,attribute:"lvl",reward:30},
+    {id: "lvl15",name: "You can call me pigeon master.",description:"Get to lvl 15", value:15,attribute:"lvl",reward:40},
+    {id: "lvl20",name: "They say this game is stupid.",description:"Get to lvl 20", value:20,attribute:"lvl",reward:50},
+    {id: "lvl25",name: "Why ?",description:"Get to lvl 25", value:25,attribute:"lvl",reward:60},
+    {id: "lvl30",name: "What am I doing with my life...",description:"Get to lvl 30", value:30,attribute:"lvl",reward:70},
+    
+    {id: "legendary1",name: "My precious !",description:"Get 1 legendary pigeon (of your level)", value:1,attribute:"legendaries",reward:10},
+    {id: "legendary10",name: "Wow legendary !",description:"Get 10 legendary pigeons (of your level)", value:10,attribute:"legendaries",reward:20},
+    {id: "legendary50",name: "Get lucky",description:"Get 50 legendary pigeons (of your level)", value:50,attribute:"legendaries",reward:30},
+    {id: "legendary100",name: "I should buy lottery tickets",description:"Get 100 legendary pigeons (of your level)", value:100,attribute:"legendaries",reward:40},
+    {id: "legendary500",name: "I did this without cheating ?",description:"Get 500 legendary pigeons (of your level)", value:500,attribute:"legendaries",reward:50},
+
+    {id: "droppingsminute20",name: "Beginning of a fortune",description:"Reach 20 droppings per minute", value:20,attribute:"droppingsminute",reward:10},
+    {id: "droppingsminute100",name: "I should build a house",description:"Reach 100 droppings per minute", value:100,attribute:"droppingsminute",reward:20},
+    {id: "droppingsminute500",name: "That's a lot of poop",description:"Reach 500 droppings per minute", value:500,attribute:"droppingsminute",reward:30},
+    {id: "droppingsminute1000",name: "Where will I store all this ?",description:"Reach 1000 droppings per minute", value:1000,attribute:"droppingsminute",reward:40},
+    {id: "droppingsminute5000",name: "Should I go to the vet ?",description:"Reach 5000 droppings per minute", value:5000,attribute:"droppingsminute",reward:50},
+    {id: "droppingsminute10000",name: "Yes, guano",description:"Reach 10000 droppings per minute", value:10000,attribute:"droppingsminute",reward:60},
+    {id: "droppingsminute20000",name: "Please stop",description:"Reach 20000 droppings per minute", value:20000,attribute:"droppingsminute",reward:70},
+
+    {id: "militaryscore10",name: "Noob",description:"Reach a military score of 10", value:10,attribute:"militaryscore",reward:10},
+    {id: "militaryscore30",name: "Ranking up",description:"Reach a military score of 30", value:30,attribute:"militaryscore",reward:20},
+    {id: "militaryscore60",name: "Not bad for a pigeon guy",description:"Reach a military score of 60", value:60,attribute:"militaryscore",reward:30},
+    {id: "militaryscore100",name: "Military strategist",description:"Reach a military score of 100", value:100,attribute:"militaryscore",reward:40},
+    {id: "militaryscore150",name: "My OP pigeons",description:"Reach a military score of 150", value:150,attribute:"militaryscore",reward:50},
+    {id: "militaryscore200",name: "Top of the world",description:"Reach a military score of 200", value:200,attribute:"militaryscore",reward:60},
+    {id: "militaryscore500",name: "Is this even possible ?",description:"Reach a military score of 500", value:500,attribute:"militaryscore",reward:70},
+
+    {id: "sell5",name: "Yes, feathers !",description:"Sell 5 pigeons", value:5,attribute:"soldpigeons",reward:10},
+    {id: "sell50",name: "I could make pillows",description:"Sell 50 pigeons", value:50,attribute:"soldpigeons",reward:20},
+    {id: "sell500",name: "My heart, my soul.",description:"Sell 500 pigeons", value:500,attribute:"soldpigeons",reward:30},
+    {id: "sell5000",name: "You monster...",description:"Sell 5000 pigeons", value:5000,attribute:"soldpigeons",reward:40},
+
+    {id: "event1",name: "At least you tried",description:"Participate in an event", value:1,attribute:"eventparticipation",reward:10},
+    {id: "eventtop3",name: "I'm not useless",description:"Finish top 3 in an event", value:2,attribute:"eventparticipation",reward:20},
+    {id: "eventtop1",name: "We are the champions",description:"Finish top 1 in an event", value:3,attribute:"eventparticipation",reward:30},
+
+    {id: "attack5",name: "Stealing those droppings !",description:"Do 5 succcessful attacks", value:5,attribute:"totalwinattacks",reward:10},
+    {id: "attack50",name: "Aggressive much ?",description:"Do 50 succcessful attacks", value:50,attribute:"totalwinattacks",reward:20},
+    {id: "attack500",name: "I should call the police",description:"Do 500 succcessful attacks", value:500,attribute:"totalwinattacks",reward:30},
+
+    {id: "defend5",name: "Me and my tough pigeons",description:"Do 5 succcessful defenses", value:5,attribute:"totalwindefences",reward:10},
+    {id: "defend50",name: "You shall not pass !",description:"Do 50 succcessful defenses", value:50,attribute:"totalwindefences",reward:20},
+    {id: "defend500",name: "Stop attacking me please.",description:"Do 500 succcessful defenses", value:500,attribute:"totalwindefences",reward:30},
+
+]
+    export default achievementslist ;
--- src/app/lists/tradeList.ts
+++ src/app/lists/tradeList.ts
@@ -1,7 +1,7 @@
 const tradeList = [
     {lvl: 0,fromHonor: 0,toFeathers:0,fromFeathers: 0,toDroppings:0 },
-    {lvl: 1,fromHonor: 10,toFeathers:0,fromFeathers: 30,toDroppings:0 },
-    {lvl: 2,fromHonor: 10,toFeathers:0,fromFeathers: 0,toDroppings:0 },
-    {lvl: 3,fromHonor: 10,toFeathers:0,fromFeathers: 0,toDroppings:0 },
+    {lvl: 1,fromHonor: 1,toFeathers:10,fromFeathers: 10,toDroppings:50 },
+    {lvl: 2,fromHonor: 2,toFeathers:100,fromFeathers: 20,toDroppings:200 },
+    {lvl: 3,fromHonor: 3,toFeathers:300,fromFeathers: 30,toDroppings:500 },
 ]
 export default tradeList;
