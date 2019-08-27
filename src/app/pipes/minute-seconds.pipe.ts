49c4f217999fa5ddda0c673a4a44fa29ba57e9bb
ArtrCtrs <arthur.catrisse@hotmail.com>
2019-08-27 15:32:36 +0200

    added minuteseconds pipe

---
+++ src/app/pipes/minute-seconds.pipe.ts
@@ -0,0 +1,14 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'minuteSeconds'
+})
+
+export class MinuteSecondsPipe implements PipeTransform {
+
+    transform(value: number): string {
+       const minutes: number = Math.floor(value / 60);
+       return minutes + ':' + (value - minutes * 60);
+    }
+
+}
